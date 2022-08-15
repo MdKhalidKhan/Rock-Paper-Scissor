c439f3b6f60f9d0e1269e07160a3a75b53c5d235
Raj <rajkhulna@outlook.com>
2022-08-15 13:15:48 +0600

    Initial Commit

---
+++ Rock Paper Scissor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissor
+//
+//  Created by Khalid Khan on 15/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock Paper Scissor/Rock_Paper_ScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorApp.swift
+//  Rock Paper Scissor
+//
+//  Created by Khalid Khan on 15/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
